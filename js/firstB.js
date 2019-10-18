a759160e67ae655f6589f7938f2da9711c483110
wishsky13 <wishsky13@gmail.com>
2019-10-18 10:03:17 +0800

    update header

--- js/firstB.js
+++ js/firstB.js
@@ -1,40 +1,14 @@
 $(document).ready(function(){
   
-    var collapse = $(".footer-collapse");
-    var collapseOpen;
-    
-    function collapseOpen(){
-      menu.css("left", "0px");
-      navbg.css("display", "block");
-      overscrolly.css("overflow","hidden")
-      login.removeClass("button");
-      menuOpen = true;
-    }
-    
-    function collapseClose(){
-      menu.css("left", "-320px");
-      navbg.css("display", "none");
-      overscrolly.css("overflow","scroll")
-      menuOpen = false;
-    }
-    
-    function toggleMenu(){
-      if (menuOpen) {
-        collapseClose();
-      } else {
-        collapseOpen();
-      }
-    }
-        close.on({
-            click: function(){
-            toggleMenu();
-            }
-        });
+    v$(function(){
+      var sideslider = $('[data-toggle=collapse-side]');
+      var get_sidebar = sideslider.attr('data-target-sidebar');
+      var get_content = sideslider.attr('data-target-content');
 
-        hamburger.on({
-            click: function(){
-            toggleMenu();
-            }
-        })
-  
-  });
+        sideslider.click(function(event){
+          $(get_sidebar).toggleClass('in');
+          $(get_content).toggleClass('out');
+        });
+      });
+
+});
