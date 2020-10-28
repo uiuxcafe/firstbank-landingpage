73471e8dac61c6f17b1e4ba3727b7841f1aa0aee
wishsky13 <wishsky13@gmail.com>
2020-10-28 16:59:27 +0800

    test game

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cdb93de4dcff69b5ef250155ff829b0.js"
+  "/precache-manifest.60347fa93cb9eef72bf38004468eff69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
---
+++ build2/env.js
@@ -0,0 +1,6 @@
+const ENV = {
+    MASTER_API_URL: 'https://rainbow-numen.herokuapp.com/v1/graphql',
+    DEV_API_URL: 'https://rainbow-numen-dev.herokuapp.com/v1/graphql'
+}
+
+window.__env__= ENV;
